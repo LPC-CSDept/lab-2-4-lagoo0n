f0166a04dc7af521bd26052a2a095e1c9cd0dfd3
lagoo0n <loganchook@gmail.com>
2023-06-24 01:40:59 -0700

    new work

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # Comlete your code here
     ##################################################
 
+    sub1 = original_str[:6]
+    sub2 = original_str[-12:]
+    merged_str = sub1 + sub2
+
     print(sub2)
     print(sub1)
     print(merged_str)
